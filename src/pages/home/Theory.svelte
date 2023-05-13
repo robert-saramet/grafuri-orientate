786e0bf0df4ad5b94e91329543d83114897270ae
Robert Saramet <robertsaramet@pm.me>
2023-05-14 02:31:54 +0300

    Shorten theory page description

--- src/pages/home/Theory.svelte
+++ src/pages/home/Theory.svelte
@@ -9,11 +9,7 @@
   title="Teorie"
   description="Această secțiune explică noțiunile de bază ale grafurilor, 
   cu accent pe extremitățile unui arc, vârfurile adiacente și incidența. 
-  Extremitățile unui arc sunt nodurile care îl definesc. Mai specific, 
-  nodul inițial al arcului se numește extremitate inițială, în timp ce 
-  nodul final se numește extremitate finală. Dacă există un arc între 
-  două noduri, acestea se numesc noduri adiacente. În ceea ce privește 
-  incidența, două arce sunt considerate incidente dacă au cel puțin o 
-  extremitate comună. De asemenea, un nod și un arc sunt considerate 
-  incidente dacă acel nod face parte din extremitatea arcului."
+  Extremitățile unui arc sunt nodurile care îl definesc. Dacă există un
+  arc între două noduri, acestea se numesc noduri adiacente. Două arce
+  sunt considerate incidente dacă au cel puțin o extremitate comună."
 />
