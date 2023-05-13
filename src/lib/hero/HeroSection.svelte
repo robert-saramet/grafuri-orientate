6755b7f2f233345580f2b952673781678f72c9f6
Robert Saramet <robertsaramet@pm.me>
2023-05-14 01:23:19 +0300

    Add typewriter animation to hero component

--- src/lib/hero/HeroSection.svelte
+++ src/lib/hero/HeroSection.svelte
@@ -3,16 +3,37 @@
   export let rating;
   export let title;
   export let description;
+  export let disabled = true;
 
   import Rating from "./Rating.svelte";
+  import TypeWriter from "svelte-typewriter";
+  
+  $: interval = 3000 / description.length;
+
+  // Check if element is in viewport
+  function actionWhenInViewport(e) {
+  const observer = new IntersectionObserver(entries => {
+    if(entries[0].isIntersecting) {
+      // Element in viewport
+      disabled = false;
+    }
+  });
+
+  observer.observe(e);
+
+}
 </script>
 
 <div class="hero min-h-screen bg-base-200">
   <div class="hero-content flex-col lg:flex-row gap-x-32 gap-y-12">
     <ImageSvg />
-    <div>
-      <h1 class="text-5xl font-bold">{title}</h1>
-      <p class="py-6 max-w-3xl">{description}</p>
+    <div use:actionWhenInViewport>
+      <TypeWriter mode="scramble" scrambleDuration={800} scrambleSlowdown={true} {disabled}>
+        <h1 class="text-5xl font-bold">{title}</h1>
+      </TypeWriter>
+      <TypeWriter {interval} keepCursorOnFinish={1000} {disabled}>
+        <p class="py-6 max-w-3xl">{description}</p>
+      </TypeWriter>
       <div class="flex flex-row gap-8">
         <button class="btn btn-primary">Află mai mult</button>
         <Rating {rating} />
