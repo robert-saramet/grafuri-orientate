da824f5c0441a9f0cca594310646519f3ad2a4f7
Robert Saramet <robertsaramet@pm.me>
2023-05-14 21:09:09 +0300

    Add helper sidebar

--- src/lib/hero/HeroSection.svelte
+++ src/lib/hero/HeroSection.svelte
@@ -7,31 +7,47 @@
 
   import Rating from "./Rating.svelte";
   import TypeWriter from "svelte-typewriter";
-  
+
   $: interval = 3000 / description.length;
+  $: showHelp = false;
 
   // Check if element is in viewport
   function actionWhenInViewport(e) {
-  const observer = new IntersectionObserver(entries => {
-    if(entries[0].isIntersecting) {
-      // Element in viewport
-      disabled = false;
-    }
-  });
+    const observer = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        // Element in viewport
+        disabled = false;
+      }
+    });
+    observer.observe(e);
+  }
 
-  observer.observe(e);
-
-}
+  // Show help after 3 seconds from full render
+  function startShowHelp() {
+    setTimeout(() => {
+      showHelp = true;
+    }, 3000);
+  }
 </script>
 
 <div class="hero min-h-screen bg-base-200">
   <div class="hero-content flex-col lg:flex-row gap-x-32 gap-y-12">
     <ImageSvg />
     <div use:actionWhenInViewport>
-      <TypeWriter mode="scramble" scrambleDuration={800} scrambleSlowdown={true} {disabled}>
+      <TypeWriter
+        mode="scramble"
+        scrambleDuration={800}
+        scrambleSlowdown={true}
+        {disabled}
+      >
         <h1 class="text-5xl font-bold">{title}</h1>
       </TypeWriter>
-      <TypeWriter {interval} keepCursorOnFinish={2000} {disabled}>
+      <TypeWriter
+        {interval}
+        keepCursorOnFinish={2000}
+        {disabled}
+        on:done={startShowHelp}
+      >
         <p class="py-6 max-w-3xl">{description}</p>
       </TypeWriter>
       <div class="flex flex-row gap-8">
@@ -41,3 +57,12 @@
     </div>
   </div>
 </div>
+
+{#if showHelp}
+  <div class="divider divider-horizontal mx-0 bg-base-300 w-3 animate-pulse" />
+  <div class="border-r flex items-center justify-center w-12 -ml-2 bg-base-200">
+    <span class="rotate-90 whitespace-nowrap font-light animate-pulse"
+      >Glisați la dreapta</span
+    >
+  </div>
+{/if}
