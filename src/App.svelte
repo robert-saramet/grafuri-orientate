0d24b58bb12d4db3c1c92f197ab08f940de7d3e1
Robert Saramet <robertsaramet@pm.me>
2023-05-15 18:54:32 +0300

    Render pages dynamically

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,34 @@
 <script>
   import Navbar from "@/lib/components/Navbar.svelte";
   import Footer from "@/lib/components/Footer.svelte";
-  import CookieBanner from "@/lib/components/CookieBanner.svelte";
+  import BottomButtons from "@/lib/components/BottomButtons.svelte";
 
   import Landing from "@/pages/home/Landing.svelte";
   import Theory from "@/pages/home/Theory.svelte";
+  import Problems from "@/pages/home/Problems.svelte";
+
+  const pages = [Landing, Theory, Problems];
+  const ids = ["landing", "theory", "problems"];
+
+  $: visibleId = ids[0];
+  function updateVisibleId(id) {
+    console.log("updateVisibleId:" + id);
+    visibleId = id;
+  }
 </script>
 
 <main class="overflow-x-hidden">
   <Navbar />
-  <div class="w-screen carousel carousel-horizontal rounded-box">
-    <div class="carousel-item w-full"><Landing /></div>
-    <div class="carousel-item w-full"><Theory /></div>
+  <div class="w-screen carousel rounded-box">
+    {#each pages as page, i}
+      <div class="carousel-item w-full" id={ids[i]}>
+        <svelte:component
+          this={page}
+          on:visibleEvent={() => updateVisibleId(ids[i])}
+        />
+      </div>
+    {/each}
   </div>
-  <CookieBanner />
+    <BottomButtons {ids} {visibleId}/>
   <Footer />
 </main>
